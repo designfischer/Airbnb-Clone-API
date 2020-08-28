e77d2f1591e2d96db08952973c096ebedb7d5354
Rafael Fischer <47259718+designfischer@users.noreply.github.com>
2020-08-28 09:48:01 -0300

    update readme

--- README.md
+++ README.md
@@ -20,4 +20,44 @@ npm run dev (development mode)
 
 ## 2 - Endpoints
 
+### Base URL
+
+localhost:3333
+
+### Creating an user
+
+POST - BaseURL/users
+
+### Listing an user by email
+
+GET - BaseURL/users/:user_email
+
+### Deleting an user
+
+DELETE - BaseURL/users/:user_id
+
+### Listing all users
+
+GET - BaseURL/users
+
+### Creating a place to rent
+
+POST - BaseURL/:user_id/places
+
+### Listing all places of an user
+
+GET - BaseURL/:user_id/places
+
+### Listing all places
+
+GET - BaseURL/places
+
+### Listing specific place
+
+GET - BaseURL/places/:place_id
+
+### Searching places by City and Type
+
+GET - Baseurl/searchplaces
+
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,25 +4,25 @@ const uploadConfig = require('../config/uploadConfig')
 
 const UserController = require('../controllers/UserController')
 const UsersController = require('../controllers/UsersController')
-const PlaceController = require('../controllers/PlaceController')
+const UserPlaceController = require('../controllers/UserPlaceController')
 const SearchPlacesController = require('../controllers/SearchPlacesController')
 const PlacesController = require('../controllers/PlacesController')
 
 const routes = Router()
 const upload = multer(uploadConfig)
 
-routes.post('/user', UserController.create)
-routes.get('/user/:user_email', UserController.read)
-routes.delete('/user/:user_id', UserController.delete)
+routes.post('/users', UserController.create)
+routes.get('/users/:user_email', UserController.read)
+routes.delete('/users/:user_id', UserController.delete)
 
-routes.get('/user', UsersController.read)
+routes.get('/users', UsersController.read)
 
-routes.post('/:user_id/place', upload.single('thumbnail'), PlaceController.create)
-routes.get('/:user_id/place', PlaceController.index)
+routes.post('/:user_id/places', upload.single('thumbnail'), UserPlaceController.create)
+routes.get('/:user_id/places', UserPlaceController.index)
 
 routes.get('/places', PlacesController.readAll)
-routes.get('/place/:place_id', PlacesController.readSpecific)
+routes.get('/places/:place_id', PlacesController.readSpecific)
 
-routes.get('/place', SearchPlacesController.read)
+routes.get('/searchplaces', SearchPlacesController.read)
 
 module.exports = routes
